042f1ec2ed42c1ac37a8c7f978e6f19356f39a80
임동우 <l01092452614@gmail.com>
2019-06-28 14:38:06 +0900

    BOJ #1550번 소스 업로드
    C

---
+++ Sources/1550/1550.c
@@ -0,0 +1,19 @@
+/**
+ *  BOJ 1550번 C언어 소스 코드
+ *  작성자 : 동동매니저 (DDManager)
+ *
+ *  ※ 실행 결과
+ *  사용 메모리 : 1,116 KB / 131,072 KB
+ *  소요 시간 : 0 ms / 2,000 ms
+ *
+ *  Copyright 2019. DDManager all rights reserved.
+ */
+
+#include <stdio.h>
+
+int main(void){
+	int n;
+	scanf("%x",&n);
+	printf("%d\n",n);
+	return 0;
+}
