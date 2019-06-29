1e06b0862ba16bbf0994282c8062e884d668cce6
임동우 <l01092452614@gmail.com>
2019-06-29 19:54:55 +0900

    BOJ #2557번 소스 업로드
    C, Pascal, Text

---
+++ Sources/2557/2557.pas
@@ -0,0 +1,12 @@
+(*
+ *  BOJ 2557번 Pascal 소스 코드
+ *  작성자 : 동동매니저 (DDManager)
+ *
+ *  ※ 실행 결과
+ *  사용 메모리 : 340 KB / 131,072 KB
+ *  소요 시간 : 0 ms / 1,000 ms
+ *
+ *  Copyright 2019. DDManager all rights reserved.
+ *)
+
+BEGIN WRITE('Hello World!')END.
