984e9ebfbb4115fc4486d5bb113eb746844b9155
임동우 <l01092452614@gmail.com>
2019-06-29 20:02:30 +0900

    BOJ #2558번 소스 업로드
    C, Pascal

---
+++ Sources/2558/2558.c
@@ -0,0 +1,20 @@
+/**
+ *  BOJ 2558번 C언어 소스 코드
+ *  작성자 : 동동매니저 (DDManager)
+ *
+ *  ※ 실행 결과
+ *  사용 메모리 : 1,116 KB / 131,072 KB
+ *  소요 시간 : 0 ms / 1,000 ms
+ *
+ *  Copyright 2019. DDManager all rights reserved.
+ */
+
+#include <stdio.h>
+
+int main(void){
+	int a,b;
+	scanf("%d",&a);
+	scanf("%d",&b);
+	printf("%d\n",a+b);
+	return 0;
+}
