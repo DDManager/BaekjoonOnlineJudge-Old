ad638c58a18fe308a8f0df88f41ced7d76ac18b8
임동우 <l01092452614@gmail.com>
2019-06-29 19:46:25 +0900

    BOJ #2556번 소스 업로드
    C, Pascal

---
+++ Sources/2556/2556.c
@@ -0,0 +1,22 @@
+/**
+ *  BOJ 2556번 C언어 소스 코드
+ *  작성자 : 동동매니저 (DDManager)
+ *
+ *  ※ 실행 결과
+ *  사용 메모리 : 1,116 KB / 131,072 KB
+ *  소요 시간 : 0 ms / 1,000 ms
+ *
+ *  Copyright 2019. DDManager all rights reserved.
+ */
+
+#include <stdio.h>
+
+int main(void){
+	int N,i,j;
+	scanf("%d",&N);
+	for(i=0;i<N;i++){
+		for(j=0;j<N;j++) putchar('*');
+		puts("");
+	}
+	return 0;
+}
