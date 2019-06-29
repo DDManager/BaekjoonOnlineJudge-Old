d02fd7ca90761196933d28d76ccbca82088c52d3
임동우 <l01092452614@gmail.com>
2019-06-29 10:12:45 +0900

    BOJ #2439번 소스 업로드
    C

---
+++ Sources/2439/2439.c
@@ -0,0 +1,23 @@
+/**
+ *  BOJ 2439번 C언어 소스 코드
+ *  작성자 : 동동매니저 (DDManager)
+ *
+ *  ※ 실행 결과
+ *  사용 메모리 : 1,116 KB / 131,072 KB
+ *  소요 시간 : 0 ms / 1,000 ms
+ *
+ *  Copyright 2019. DDManager all rights reserved.
+ */
+
+#include <stdio.h>
+
+int main(void){
+	int N,i,j;
+	scanf("%d",&N);
+	for(i=1;i<=N;i++){
+		for(j=i;j<N;j++) putchar(' ');
+		for(j=0;j<i;j++) putchar('*');
+		puts("");
+	}
+	return 0;
+}
