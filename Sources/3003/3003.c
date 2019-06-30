da5118408161644c82efa293312ed7d3227b80bc
임동우 <l01092452614@gmail.com>
2019-06-30 13:20:49 +0900

    BOJ #3003번 소스 업로드
    C

---
+++ Sources/3003/3003.c
@@ -0,0 +1,19 @@
+/**
+ *  BOJ 3003번 C언어 소스 코드
+ *  작성자 : 동동매니저 (DDManager)
+ *
+ *  ※ 실행 결과
+ *  사용 메모리 : 1,116 KB / 131,072 KB
+ *  소요 시간 : 0 ms / 1,000 ms
+ *
+ *  Copyright 2019. DDManager all rights reserved.
+ */
+
+#include <stdio.h>
+
+int main(void){
+	int a,b,c,d,e,f;
+	scanf("%d %d %d %d %d %d",&a,&b,&c,&d,&e,&f);
+	printf("%d %d %d %d %d %d\n",1-a,1-b,2-c,2-d,2-e,8-f);
+	return 0;
+}
