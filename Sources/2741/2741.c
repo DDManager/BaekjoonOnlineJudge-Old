5f2b85d05ef7819bb28c4283eb8b7644a0dc6b23
임동우 <l01092452614@gmail.com>
2019-06-30 10:45:08 +0900

    BOJ #2741번 소스 업로드
    C, Pascal

---
+++ Sources/2741/2741.c
@@ -0,0 +1,19 @@
+/**
+ *  BOJ 2741번 C언어 소스 코드
+ *  작성자 : 동동매니저 (DDManager)
+ *
+ *  ※ 실행 결과
+ *  사용 메모리 : 1,116 KB / 131,072 KB
+ *  소요 시간 : 12 ms / 1,000 ms
+ *
+ *  Copyright 2019. DDManager all rights reserved.
+ */
+
+#include <stdio.h>
+
+int main(void){
+	int n,i;
+	scanf("%d",&n);
+	for(i=1;i<=n;i++) printf("%d\n",i);
+	return 0;
+}
---
+++ Sources/2741/2741.pas
@@ -0,0 +1,16 @@
+(*
+ *  BOJ 2741번 Pascal 소스 코드
+ *  작성자 : 동동매니저 (DDManager)
+ *
+ *  ※ 실행 결과
+ *  사용 메모리 : 340 KB / 131,072 KB
+ *  소요 시간 : 92 ms / 1,000 ms
+ *
+ *  Copyright 2019. DDManager all rights reserved.
+ *)
+
+VAR N,L:LONGINT;
+BEGIN
+	READ(N);
+	FOR L:=1 TO N DO WRITELN(L);
+END.
