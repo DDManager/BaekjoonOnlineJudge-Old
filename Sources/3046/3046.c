d0f28cc31b38b7ebf2643b6efd5f12c01e3e1ad8
임동우 <l01092452614@gmail.com>
2019-06-30 13:25:25 +0900

    BOJ #3046번 소스 업로드
    C, Pascal

---
+++ Sources/3046/3046.c
@@ -0,0 +1,19 @@
+/**
+ *  BOJ 3046번 C언어 소스 코드
+ *  작성자 : 동동매니저 (DDManager)
+ *
+ *  ※ 실행 결과
+ *  사용 메모리 : 1,116 KB / 131,072 KB
+ *  소요 시간 : 0 ms / 1,000 ms
+ *
+ *  Copyright 2019. DDManager all rights reserved.
+ */
+
+#include <stdio.h>
+
+int main(void){
+	int r,s;
+	scanf("%d %d",&r,&s);
+	printf("%d\n",s*2-r);
+	return 0;
+}
