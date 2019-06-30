77b817c7cf7af379be75085e0602d189bbd36b25
임동우 <l01092452614@gmail.com>
2019-06-30 10:59:09 +0900

    BOJ #2742번 소스 업로드
    C, Pascal

---
+++ Sources/2742/2742.pas
@@ -0,0 +1,16 @@
+(*
+ *  BOJ 2742번 Pascal 소스 코드
+ *  작성자 : 동동매니저 (DDManager)
+ *
+ *  ※ 실행 결과
+ *  사용 메모리 : 340 KB / 131,072 KB
+ *  소요 시간 : 96 ms / 1,000 ms
+ *
+ *  Copyright 2019. DDManager all rights reserved.
+ *)
+
+VAR N,L:LONGINT;
+BEGIN
+	READ(N);
+	FOR L:=N DOWNTO 1 DO WRITELN(L);
+END.
