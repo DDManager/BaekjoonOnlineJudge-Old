6e99aefa44e7d2144d9f7af3f8f27a89dab377a2
임동우 <l01092452614@gmail.com>
2019-07-01 20:13:09 +0900

    BOJ #10869번 소스 업로드
    C, Pascal

---
+++ Sources/10869/10869.c
@@ -0,0 +1,23 @@
+/**
+ *  BOJ 10869번 C언어 소스 코드
+ *  작성자 : 동동매니저 (DDManager)
+ *
+ *  ※ 실행 결과
+ *  사용 메모리 : 1,116 KB / 262,144 KB
+ *  소요 시간 : 0 ms / 1,000 ms
+ *
+ *  Copyright 2019. DDManager all rights reserved.
+ */
+
+#include <stdio.h>
+
+int main(void){
+	int a,b;
+	scanf("%d %d",&a,&b);
+	printf("%d\n",a+b);
+	printf("%d\n",a-b);
+	printf("%d\n",a*b);
+	printf("%d\n",a/b);
+	printf("%d\n",a%b);
+	return 0;
+}
