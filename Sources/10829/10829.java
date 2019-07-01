17fedeb803effb860951a886aa854282f34b546f
임동우 <l01092452614@gmail.com>
2019-07-01 19:40:14 +0900

    BOJ #10829번 소스 업로드
    Java, Kotlin (Native)

---
+++ Sources/10829/10829.java
@@ -0,0 +1,19 @@
+/**
+ *  BOJ 10829번 Java 소스 코드
+ *  작성자 : 동동매니저 (DDManager)
+ *
+ *  ※ 실행 결과
+ *  사용 메모리 : 14,296 KB / 540,672 KB
+ *  소요 시간 : 112 ms / 3,000 ms
+ *
+ *  Copyright 2019. DDManager all rights reserved.
+ */
+
+import java.util.*;
+public class Main{
+	public static void main(String args[]) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println(Long.toString(sc.nextLong(),2));
+		sc.close();
+	}
+}
---
+++ Sources/10829/10829.kt
@@ -0,0 +1,14 @@
+/**
+ *  BOJ 10829번 Kotlin (Native) 소스 코드
+ *  작성자 : 동동매니저 (DDManager)
+ *
+ *  ※ 실행 결과
+ *  사용 메모리 : 14,200 KB / 278,528 KB
+ *  소요 시간 : 4 ms / 1,000 ms
+ *
+ *  Copyright 2019. DDManager all rights reserved.
+ */
+
+fun main(args:Array<String>){
+	println(readLine()!!.toLong().toString(2))
+}
