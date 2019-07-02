da6a49f4262b0008756faf60adae372ec3ea5515
임동우 <l01092452614@gmail.com>
2019-07-02 12:18:01 +0900

    BOJ #11718번 소스 업로드
    Pascal

---
+++ Sources/11718/11718.pas
@@ -0,0 +1,19 @@
+(*
+ *  BOJ 11718번 Pascal 소스 코드
+ *  작성자 : 동동매니저 (DDManager)
+ *
+ *  ※ 실행 결과
+ *  사용 메모리 : 340 KB / 262,144 KB
+ *  소요 시간 : 0 ms / 1,000 ms
+ *
+ *  Copyright 2019. DDManager all rights reserved.
+ *)
+
+VAR S:STRING[100];
+BEGIN
+	WHILE NOT EOF(INPUT)DO
+	BEGIN
+		READLN(S);
+		WRITELN(S);
+	END;
+END.
