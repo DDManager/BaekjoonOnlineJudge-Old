d77daad1bfa5e18ee897071317e578d47b593326
임동우 <l01092452614@gmail.com>
2019-07-02 12:08:34 +0900

    BOJ #11506번 소스 업로드
    C, Pascal, Text

---
+++ Sources/11506/11506.c
@@ -0,0 +1,18 @@
+/**
+ *  BOJ 11506번 C언어 소스 코드
+ *  작성자 : 동동매니저 (DDManager)
+ *
+ *  ※ 실행 결과
+ *  사용 메모리 : 1,116 KB / 262,144 KB
+ *  소요 시간 : 0 ms / 1,000 ms
+ *
+ *  Copyright 2019. DDManager all rights reserved.
+ */
+
+#include <stdio.h>
+
+int main(void){
+	char c[]={0xEF,0xBF,0xBD,0};
+	puts(c);
+	return 0;
+}
