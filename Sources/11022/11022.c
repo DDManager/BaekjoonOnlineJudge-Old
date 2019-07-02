ec021efd73560d373af6827ac7378ce39f3291f5
임동우 <l01092452614@gmail.com>
2019-07-02 11:43:54 +0900

    BOJ #11022번 소스 업로드
    C

---
+++ Sources/11022/11022.c
@@ -0,0 +1,22 @@
+/**
+ *  BOJ 11022번 C언어 소스 코드
+ *  작성자 : 동동매니저 (DDManager)
+ *
+ *  ※ 실행 결과
+ *  사용 메모리 : 1,116 KB / 262,144 KB
+ *  소요 시간 : 0 ms / 1,000 ms
+ *
+ *  Copyright 2019. DDManager all rights reserved.
+ */
+
+#include <stdio.h>
+
+int main(void){
+	int T,A,B,i;
+	scanf("%d",&T);
+	for(i=1;i<=T;i++){
+		scanf("%d %d",&A,&B);
+		printf("Case #%d: %d + %d = %d\n",i,A,B,A+B);
+	}
+	return 0;
+}
