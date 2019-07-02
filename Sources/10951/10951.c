968dd6c4c43fd42687c76218d666373b1f2ed89c
임동우 <l01092452614@gmail.com>
2019-07-02 10:33:28 +0900

    BOJ #10951번 소스 업로드
    C

---
+++ Sources/10951/10951.c
@@ -0,0 +1,18 @@
+/**
+ *  BOJ 10951번 C언어 소스 코드
+ *  작성자 : 동동매니저 (DDManager)
+ *
+ *  ※ 실행 결과
+ *  사용 메모리 : 1,116 KB / 262,144 KB
+ *  소요 시간 : 0 ms / 1,000 ms
+ *
+ *  Copyright 2019. DDManager all rights reserved.
+ */
+
+#include <stdio.h>
+
+int main(void){
+	int A,B;
+	while(scanf("%d %d",&A,&B)!=EOF) printf("%d\n",A+B);
+	return 0;
+}
