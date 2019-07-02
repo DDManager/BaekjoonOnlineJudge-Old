af2cd3ec00c8db1cd4c61fc30bd7a7094ae381cb
임동우 <l01092452614@gmail.com>
2019-07-02 10:20:14 +0900

    BOJ #10889번 소스 업로드
    C, Pascal

---
+++ Sources/10889/10889.c
@@ -0,0 +1,19 @@
+/**
+ *  BOJ 10889번 C언어 소스 코드
+ *  작성자 : 동동매니저 (DDManager)
+ *
+ *  ※ 실행 결과
+ *  사용 메모리 : 1,116 KB / 2,048 KB
+ *  소요 시간 : 0 ms / 1,000 ms
+ *
+ *  Copyright 2019. DDManager all rights reserved.
+ */
+
+#include <stdio.h>
+
+int main(void){
+	int x;
+	scanf("%d",&x);
+	printf("%d\n",111*x*x+11*x+1);
+	return 0;
+}
