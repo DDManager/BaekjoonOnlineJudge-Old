e520ddde27724c96a8eb400f45160ed4c6e7aad4
임동우 <l01092452614@gmail.com>
2019-07-05 11:22:01 +0900

    BOJ #2163번 소스 업로드
    C, Pascal

---
+++ Sources/2163/2163.c
@@ -0,0 +1,19 @@
+/**
+ *  BOJ 2163번 C언어 소스 코드
+ *  작성자 : 동동매니저 (DDManager)
+ *
+ *  ※ 실행 결과
+ *  사용 메모리 : 1,116 KB / 131,072 KB
+ *  소요 시간 : 0 ms / 2,000 ms
+ *
+ *  Copyright 2019. DDManager all rights reserved.
+ */
+
+#include <stdio.h>
+
+int main(void){
+	int N,M;
+	scanf("%d %d",&N,&M);
+	printf("%d\n",N*M-1);
+	return 0;
+}
---
+++ Sources/2163/2163.pas
@@ -0,0 +1,16 @@
+(*
+ *  BOJ 2163번 Pascal 소스 코드
+ *  작성자 : 동동매니저 (DDManager)
+ *
+ *  ※ 실행 결과
+ *  사용 메모리 : 340 KB / 131,072 KB
+ *  소요 시간 : 0 ms / 2,000 ms
+ *
+ *  Copyright 2019. DDManager all rights reserved.
+ *)
+
+VAR N,M:LONGINT;
+BEGIN
+	READ(N,M);
+	WRITE(N*M-1);
+END.
