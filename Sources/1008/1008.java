dc125db9ea308fd57e918551ae6948c7e8b75b2d
임동우 <l01092452614@gmail.com>
2019-07-26 11:40:04 +0900

    BOJ #1008번 소스 업로드 (Java)

---
+++ Sources/1008/1008.java
@@ -0,0 +1,19 @@
+/**
+ *  BOJ 1008번 Java 소스 코드
+ *  작성자 : 동동매니저 (DDManager)
+ *
+ *  ※ 실행 결과
+ *  사용 메모리 : 16,048 KB / 278,528 KB
+ *  소요 시간 : 120 ms / 5,000 ms
+ *
+ *  Copyright 2019. DDManager all rights reserved.
+ */
+
+import java.util.Scanner;
+public class Main{
+	public static void main(String[] args){
+		Scanner sc=new Scanner(System.in);
+		System.out.println(sc.nextDouble()/sc.nextInt());
+		sc.close();
+	}
+}
