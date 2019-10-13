b56b7427388db121793c4ed9235136b862a8cddb
임동우 <l01092452614@gmail.com>
2019-10-13 15:36:50 +0900

    BOJ #2417번 소스 업로드
    C

---
+++ Sources/2417/2417.c
@@ -0,0 +1,20 @@
+/**
+ *  BOJ 2417번 C언어 소스 코드
+ *  작성자 : 동동매니저 (DDManager)
+ *
+ *  ※ 실행 결과
+ *  사용 메모리 : 1,132 KB / 131,072 KB
+ *  소요 시간 : 0 ms / 1,000 ms
+ *
+ *  Copyright 2019. DDManager all rights reserved.
+ */
+
+#include <stdio.h>
+#include <math.h>
+
+int main(void){
+	long long N;
+	scanf("%lld",&N);
+	printf("%.Lf\n",ceill(sqrtl(N)));
+	return 0;
+}
